5955656c3c59a6bd84e9d57e9127e56f91fd5eaa
kenichi beveridge <kenbev84@gmail.com>
2023-03-22 15:59:30 +0000

    Add Fizz conditional

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,8 +1,12 @@
 public class FizzBuzz {
 
     public static void main(String[] args) {
-        for (int i = 0; i <= 100; i++) {
-            System.out.println(i);
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else {
+                System.out.println(i);
+            }
         }
     }
 }
