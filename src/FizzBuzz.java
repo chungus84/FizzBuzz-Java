907b91e4db8eec9927523daff9ad7efcd99283ba
kenichi beveridge <kenbev84@gmail.com>
2023-03-22 16:19:49 +0000

    Refactored solution

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -2,15 +2,11 @@ public class FizzBuzz {
 
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
-            if (i % 15 == 0) {
-                System.out.println("FizzBuzz");
-            } else if (i % 3 == 0) {
-                System.out.println("Fizz");
-            } else if (i % 5 == 0) {
-                System.out.println("Buzz");
-            } else {
-                System.out.println(i);
-            }
+            String result = "";
+            if (i % 3 == 0) { result = "Fizz"; }
+            if (i % 5 == 0) { result += "Buzz"; }
+            if (result == "") { result = String.valueOf(i); }
+            System.out.println(result);
         }
     }
 }
